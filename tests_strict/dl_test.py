faa1b63d0cc2ef51beba0305a4c6bb5b6b4f13cc
ak11 <mark@aur.ll.to>
2017-12-06 22:43:17 +0900

    整理
    テストが時々失敗するので対策

--- tests_strict/dl_test.py
+++ tests_strict/dl_test.py
@@ -37,18 +37,18 @@ def test_xor(tmpdir):
 
     X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     y = np.array([0, 1, 1, 0])
-    lr_list = [1e-3] * 3 + [1e-4] * 1
+    lr_list = [1e-3] * 5 + [1e-4] * 3
 
     with tk.dl.session():
         inp = x = keras.layers.Input(shape=(2,))
         x = keras.layers.Dense(16, activation='relu')(x)
         x = keras.layers.Dense(1, activation='sigmoid')(x)
         model = keras.models.Model(inputs=inp, outputs=x)
-        model.compile('adam', 'binary_crossentropy', ['acc'])
+        model.compile('nadam', 'binary_crossentropy', ['acc'])
         model.fit(
-            X.repeat(3200, axis=0),
-            y.repeat(3200, axis=0),
-            epochs=100,
+            X.repeat(4096, axis=0),
+            y.repeat(4096, axis=0),
+            epochs=128,
             verbose=2,
             callbacks=[tk.dl.my_callback_factory()(str(tmpdir), lr_list=lr_list)])
         pred = model.predict(X)
