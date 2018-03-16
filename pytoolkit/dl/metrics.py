74b22815c4ad1bc629f20ae4a5c548a91d74ce06
aki <mark@aur.ll.to>
2018-03-16 21:34:18 +0900

    .

--- pytoolkit/dl/metrics.py
+++ pytoolkit/dl/metrics.py
@@ -7,7 +7,9 @@ def tp(y_true, y_pred):
     バッチごとのtrue/falseの数が一定でない限り正しく算出されないため要注意。
     """
     import keras.backend as K
-    return K.mean(K.greater_equal(y_true, 0.5) * K.greater_equal(y_pred, 0.5))
+    mask = K.cast(K.greater_equal(y_true, 0.5), K.floatx())  # true
+    pred = K.cast(K.greater_equal(y_pred, 0.5), K.floatx())  # positive
+    return K.sum(pred * mask) / K.sum(mask)
 
 
 def fp(y_true, y_pred):
@@ -16,7 +18,9 @@ def fp(y_true, y_pred):
     バッチごとのtrue/falseの数が一定でない限り正しく算出されないため要注意。
     """
     import keras.backend as K
-    return K.mean(K.less(y_true, 0.5) * K.greater_equal(y_pred, 0.5))
+    mask = K.cast(K.less(y_true, 0.5), K.floatx())  # false
+    pred = K.cast(K.greater_equal(y_pred, 0.5), K.floatx())  # positive
+    return K.sum(pred * mask) / K.sum(mask)
 
 
 # 再現率(recall)
