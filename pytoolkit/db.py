ff736941f96526645954a7edf6512b07374ede67
aki <mark@aur.ll.to>
2018-04-12 11:19:25 +0900

    wait_for_connection

--- pytoolkit/db.py
+++ pytoolkit/db.py
@@ -1,4 +1,6 @@
 """DB(主にSQLAlchemy)関連"""
+import logging
+import time
 
 from . import utils
 
@@ -15,6 +17,33 @@ def register_ping():
                 cursor.execute('SELECT 1')
             except BaseException:
                 raise sqlalchemy.exc.DisconnectionError()
+
+
+def wait_for_connection(url):
+    """DBに接続可能になるまで待機する。"""
+    import sqlalchemy
+    logger = logging.getLogger(__name__)
+    failed = False
+    while True:
+        try:
+            engine = sqlalchemy.create_engine(url)
+            try:
+                result = engine.execute('SELECT 1')
+                try:
+                    # 接続成功
+                    if failed:
+                        logger.info('DB接続成功')
+                    break
+                finally:
+                    result.close()
+            finally:
+                engine.dispose()
+        except BaseException:
+            # 接続失敗
+            if not failed:
+                failed = True
+                logger.info(f'DB接続待機中 . . . (URL: {url})')
+            time.sleep(1)
 
 
 def safe_close(session):
