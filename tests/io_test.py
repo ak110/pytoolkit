739d2b7a9a0ce7e9650c9a0e103256fd6c5b534e
aki <mark@aur.ll.to>
2018-07-13 22:54:18 +0900

    .

--- tests/io_test.py
+++ tests/io_test.py
@@ -1,18 +1,15 @@
+import pathlib
 
 import pytoolkit as tk
 
 
 def test_read_write(tmpdir):
-    path = str(tmpdir.join('read_write_test.txt'))
+    path = pathlib.Path(tmpdir.join('read_write_test.txt'))
 
-    tk.io.write_all_text(path, 'あああ\nいいい')
-    assert tk.io.read_all_text(path) == 'あああ\nいいい'
+    path.write_text('あああ\nいいい', encoding='utf-8')
+    assert path.read_text(encoding='utf-8') == 'あああ\nいいい'
     assert tk.io.read_all_lines(path) == ['あああ', 'いいい']
 
     tk.io.write_all_lines(path, ['ううう', 'えええ'])
-    assert tk.io.read_all_text(path) == 'ううう\nえええ\n'
+    assert path.read_text(encoding='utf-8') == 'ううう\nえええ\n'
     assert tk.io.read_all_lines(path) == ['ううう', 'えええ']
-    assert tk.io.read_all_bytes(path) == 'ううう\nえええ\n'.encode('utf-8')
-
-    tk.io.write_all_bytes(path, 'おおお\nかかか'.encode('utf-8'))
-    assert tk.io.read_all_text(path) == 'おおお\nかかか'
