2629d90b6ad995bfa4592aa30f94f0480d662b22
aki <mark@aur.ll.to>
2018-10-03 19:48:27 +0900

    .

--- pytoolkit/dl/layers.py
+++ pytoolkit/dl/layers.py
@@ -12,6 +12,7 @@ def get_custom_objects():
         resize2d(),
         pad2d(),
         pad_channel_2d(),
+        coord_channel_2d(),
         channel_pair_2d(),
         group_normalization(),
         destandarization(),
@@ -226,6 +227,61 @@ def pad_channel_2d():
             return dict(list(base_config.items()) + list(config.items()))
 
     return PadChannel2D
+
+
+def coord_channel_2d():
+    """CoordConvなレイヤー。
+
+    ■[1807.03247] An Intriguing Failing of Convolutional Neural Networks and the CoordConv Solution
+    https://arxiv.org/abs/1807.03247
+    """
+    import keras
+    import keras.backend as K
+    import tensorflow as tf
+
+    class CoordChannel2D(keras.layers.Layer):
+        """CoordConvなレイヤー。
+
+        ■[1807.03247] An Intriguing Failing of Convolutional Neural Networks and the CoordConv Solution
+        https://arxiv.org/abs/1807.03247
+        """
+
+        def __init__(self, x_channel=True, y_channel=True, **kwargs):
+            super().__init__(**kwargs)
+            self.x_channel = x_channel
+            self.y_channel = y_channel
+
+        def compute_output_shape(self, input_shape):
+            assert len(input_shape) == 4
+            input_shape = list(input_shape)
+            if self.x_channel:
+                input_shape[-1] += 1
+            if self.y_channel:
+                input_shape[-1] += 1
+            return tuple(input_shape)
+
+        def call(self, inputs, **kwargs):
+            input_shape = K.shape(inputs)
+            pad_shape = (input_shape[0], input_shape[1], input_shape[2], 1)
+            ones = K.ones(pad_shape)
+            pad_channels = []
+            if self.x_channel:
+                gradation = K.cast(K.arange(0, input_shape[2]), K.floatx()) / K.cast(input_shape[2], K.floatx())
+                pad_channels.append(ones * K.reshape(gradation, (1, 1, input_shape[2], 1)))
+            if self.y_channel:
+                gradation = K.cast(K.arange(0, input_shape[1]), K.floatx()) / K.cast(input_shape[1], K.floatx())
+                pad_channels.append(ones * K.reshape(gradation, (1, input_shape[1], 1, 1)))
+            return K.concatenate([inputs] + pad_channels, axis=-1)
+
+        def get_config(self):
+            config = {
+                'x_channel': self.x_channel,
+                'y_channel': self.y_channel,
+            }
+            base_config = super().get_config()
+            return dict(list(base_config.items()) + list(config.items()))
+
+    return CoordChannel2D
 
 
 def channel_pair_2d():
