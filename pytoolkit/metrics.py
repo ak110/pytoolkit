8853e65805dd2f0cae5795d59b96d6cb3815a5d3
aki <mark@aur.ll.to>
2019-03-17 22:08:28 +0900

    binary_iou

--- pytoolkit/metrics.py
+++ pytoolkit/metrics.py
@@ -41,14 +41,25 @@ def categorical_iou(target_classes=None, strict=True):
     return iou
 
 
-def mean_iou(y_true, y_pred, threhsold=0.5):
-    """Mean IoU。"""
-    y_pred_mask = tf.to_int32(y_pred >= threhsold)
-    score, up_opt = tf.metrics.mean_iou(y_true, y_pred_mask, 2)
-    K.get_session().run(tf.local_variables_initializer())
-    with tf.control_dependencies([up_opt]):
-        score = tf.identity(score)
-    return score
+def binary_iou(target_classes=None, threshold=0.5):
+    """画像ごとクラスごとのIoUを算出して平均するmetric。
+
+    Args:
+        target_classes: 対象のクラスindexの配列。Noneなら全クラス。
+        threshold: 予測の閾値
+
+    """
+    def iou(y_true, y_pred):
+        if target_classes is not None:
+            y_true = y_true[..., target_classes]
+            y_pred = y_pred[..., target_classes]
+        axes = list(range(1, K.ndim(y_true) - 1))
+        t = K.greater_equal(y_true, 0.5)
+        p = K.greater_equal(y_pred, threshold)
+        inter = K.sum(K.cast(tf.math.logical_and(t, p, name='inter'), 'float32'), axis=axes)
+        union = K.sum(K.cast(tf.math.logical_or(t, p, name='union'), 'float32'), axis=axes)
+        return inter / K.maximum(union, 1)
+    return iou
 
 
 def tpr(y_true, y_pred):
