663d227be46a9aaa80fdfc62df28d58e370505bd
aki <mark@aur.ll.to>
2019-06-14 23:09:03 +0900

    .

--- pytoolkit/preprocessing.py
+++ pytoolkit/preprocessing.py
@@ -1,6 +1,7 @@
 """前処理関連。"""
 
 import numpy as np
+import pandas as pd
 import sklearn.base
 import sklearn.preprocessing
 
@@ -8,15 +9,26 @@ import sklearn.preprocessing
 class ClipTransformer(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
     """np.clipするだけのTransformer。"""
 
-    def __init__(self, min_value=-3, max_value=+3):
-        assert min_value < max_value
-        self.min_value = min_value
-        self.max_value = max_value
+    def __init__(self, lower_percentile=1, upper_percentile=99):
+        assert 0 <= lower_percentile < upper_percentile <= 100
+        self.lower_percentile = lower_percentile
+        self.upper_percentile = upper_percentile
+        self.lower_bound_ = -np.inf
+        self.upper_bound_ = np.inf
 
     def fit(self, X, y=None):
-        del X, y
+        del y
+        self.lower_bound_, self.upper_bound_ = np.percentile(
+            X, [self.lower_percentile, self.upper_percentile], axis=0, keepdims=True
+        )
         return self
 
     def transform(self, X, y=None):
         del y
-        return np.clip(X, self.min_value, self.max_value)
+        if isinstance(X, pd.DataFrame):
+            X = X.copy()
+            X.iloc[:, :] = np.clip(X.values, self.lower_bound_, self.upper_bound_)
+            return X
+        else:
+            return np.clip(X, self.lower_bound_, self.upper_bound_)
+
