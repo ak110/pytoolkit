585b1f823b1bcb60428ab328bde9400cc7d02e08
aki <mark@aur.ll.to>
2023-02-07 19:15:29 +0900

    .

--- pytoolkit/pipelines_test.py
+++ pytoolkit/pipelines_test.py
@@ -2,18 +2,20 @@
 
 import polars as pl
 
-import pytoolkit as tk
+import pytoolkit
 
 
-class InputStep(tk.pipelines.Step):
-    def run(self, df: pl.DataFrame, run_type: tk.pipelines.RunType) -> pl.DataFrame:
+class InputStep(pytoolkit.pipelines.Step):
+    def run(
+        self, df: pl.DataFrame, run_type: pytoolkit.pipelines.RunType
+    ) -> pl.DataFrame:
         """当該ステップの処理。Pipeline経由で呼び出される。"""
         return pl.DataFrame({"run_type": [run_type] * 3})
 
 
 def test_step(tmpdir):
-    run_type: tk.pipelines.RunType = "train"
-    df = tk.pipelines.Pipeline(tmpdir / "models", tmpdir / "cache").run(
+    run_type: pytoolkit.pipelines.RunType = "train"
+    df = pytoolkit.pipelines.Pipeline(tmpdir / "models", tmpdir / "cache").run(
         InputStep, run_type
     )
     assert tuple(df["run_type"].to_list()) == tuple([run_type] * 3)
